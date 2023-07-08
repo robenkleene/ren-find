2cd3390ef0e01d18a9e1e004b6d59e5195ac1595
Roben Kleene <contact@robenkleene.com>
2023-07-08 11:31:26 +0000

    WIP Fix some compile errors

--- src/writer.rs
+++ src/writer.rs
@@ -33,7 +33,10 @@ impl<'a> Writer<'a> {
         }
 
         let modified = modified_lines.join("\n");
-        let original = self.paths.to_string_lossy().join("\n");
+        let original = self.paths.into_iter()
+            .map(|p| p.to_string_lossy())
+            .collect()
+            .join("\n");
         let patch = create_patch(&original, &modified);
         let f = match color {
             true => PatchFormatter::new().with_color(),
@@ -45,7 +48,7 @@ impl<'a> Writer<'a> {
     pub(crate) fn write_file(&self) -> Result<()> {
         for path in self.paths {
             let replaced = self.replacer.replace(path.to_string_lossy().as_bytes());
-            let result = std::str::from_utf8(&replaced);
+            let result = std::str::from_utf8(&replaced)?;
             fs::rename(path, result)?;
         }
         Ok(())
