1f4cc89c116198d4ccce683188e98f39b318158f
Roben Kleene <contact@robenkleene.com>
2023-07-10 05:41:50 -0400

    Improve patch file

--- tests/data/files/generate.sh
+++ tests/data/files/generate.sh
@@ -4,4 +4,8 @@ set -euo pipefail
 
 cd "$(dirname "$0")" || exit 1
 
-diff --unified start.txt <(sed s/changes/altered/g start.txt) > patch.patch
+diff --unified start.txt <(sed s/changes/altered/g start.txt) > patch.patch || true
+sed -i.bak '1s/.*/--- a\/original.txt/' patch.patch
+sed -i.bak '2s/.*/+++ b\/original.txt/' patch.patch
+rm patch.patch.bak
+
