696731ad5eec391e56ba3da5512034ad84e867a4
Roben Kleene <contact@robenkleene.com>
2023-07-10 05:48:09 -0400

    Work on patch file

--- tests/data/files/generate.sh
+++ tests/data/files/generate.sh
@@ -7,5 +7,8 @@ cd "$(dirname "$0")" || exit 1
 diff --unified start.txt <(sed s/changes/altered/g start.txt) > patch.patch || true
 sed -i.bak '1s/.*/--- original/' patch.patch
 sed -i.bak '2s/.*/+++ modified/' patch.patch
+
+sed -i.bak '3s#.*#@@ -1,5 +1,5 @@#' patch.patch
+
 rm patch.patch.bak
 
