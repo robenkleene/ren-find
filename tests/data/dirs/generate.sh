1d1224e39a2f6cc795829fdad5ecf6e882974ba1
Roben Kleene <contact@robenkleene.com>
2023-07-13 05:02:43 -0400

    Setup dir test

---
+++ tests/data/dirs/generate.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd "$(dirname "$0")" || exit 1
+
+diff --unified find.txt <(sed s/changes/altered/g find.txt) > patch.patch || true
+sed -i.bak '1s/.*/--- original/' patch.patch
+sed -i.bak '2s/.*/+++ modified/' patch.patch
+
+rm patch.patch.bak
+
