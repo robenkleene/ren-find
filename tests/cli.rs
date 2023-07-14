ad9a7dba1b1b238482231b0e7a61b9de6c269bfc
Roben Kleene <contact@robenkleene.com>
2023-07-14 05:27:21 -0400

    Setup test

--- tests/cli.rs
+++ tests/cli.rs
@@ -57,47 +57,21 @@ mod cli {
 
     #[test]
     fn test_moving() -> Result<()> {
-        let input = fs::read_to_string("tests/data/line-endings/grep.txt").expect("Error reading input");
-        let ending_path = Path::new("tests/data/line-endings/ending.txt");
-        let noending_path = Path::new("tests/data/line-endings/noending.txt");
-        let ending_file_name = ending_path.file_name().expect("Error getting filename");
-        let noending_file_name = noending_path.file_name().expect("Error getting filename");
+        let input = fs::read_to_string("tests/data/move/find.txt").expect("Error reading input");
+        let file_path_component = "tests/data/mov/change dir with spaces/change dir with spaces two/change file with spaces";
+        let file_path = Path::new("tests/data/mov").join(file_path_component);
         let tmp_dir = tempfile::tempdir()?;
         let tmp_dir_path = tmp_dir.path();
-        let ending_path_dst = tmp_dir_path.join(ending_file_name);
-        let noending_path_dst = tmp_dir_path.join(noending_file_name);
-        fs::copy(ending_path, &ending_path_dst).expect("Error copying file");
-        fs::copy(noending_path, &noending_path_dst).expect("Error copying file");
+        let file_path_dst = tmp_dir_path.join(file_path_component);
+        fs::copy(file_path, &file_path_dst).expect("Error copying file");
         mov()
             .current_dir(tmp_dir_path)
             .write_stdin(input)
-            .args(&["foo", "bar", "-w"])
+            .args(&["change", "altered", "-w"])
             .assert()
             .success();
-        fn has_eol(path: &PathBuf) -> std::io::Result<bool> {
-            let mut file = File::open(&path)?;
-            let mut buffer = [0; 1];
-
-            match file.seek(std::io::SeekFrom::End(-1)) {
-                Ok(_) => (),
-                // Empty file
-                Err(_) => return Ok(false),
-            }
-
-            file.read(&mut buffer[..])?;
-            if buffer == [b'\n'] {
-                return Ok(true);
-            };
-            Ok(false)
-        }
-        let ending_result = has_eol(&ending_path.to_path_buf())?;
-        let noending_result = has_eol(&noending_path.to_path_buf())?;
-        let ending_dst_result = has_eol(&ending_path_dst.to_path_buf())?;
-        let noending_dst_result = has_eol(&noending_path_dst.to_path_buf())?;
-        assert!(ending_result);
-        assert!(!noending_result);
-        assert_eq!(ending_result, ending_dst_result);
-        assert_eq!(noending_result, noending_dst_result);
+        // TODO: Test src is missing
+        // TODO; Test dst exist
         Ok(())
     }
 }
