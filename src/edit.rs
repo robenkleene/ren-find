a13113578519c2bd9b2eb49104d1cfd244795732
Roben Kleene <contact@robenkleene.com>
2023-07-21 05:59:36 -0400

    WIP Handle edits as hash

--- src/edit.rs
+++ src/edit.rs
@@ -1,19 +1,19 @@
-use std::path::PathBuf;
-use std::io::StdinLock;
+use indexmap::IndexMap;
 use std::io::prelude::*;
+use std::io::StdinLock;
+use std::path::PathBuf;
 
-pub(crate) struct Edit { }
+pub(crate) struct Edit {}
 
 impl Edit {
-    pub(crate) fn parse (
-        reader: StdinLock<'_>
-    ) -> Result<Vec<PathBuf>, std::io::Error> {
-        let mut paths = Vec::new();
+    pub(crate) fn parse(
+        reader: StdinLock<'_>,
+    ) -> Result<IndexMap<PathBuf, PathBuf>, std::io::Error> {
+        let mut path_to_path = IndexMap::new();
         for line in reader.lines() {
             let path = PathBuf::from(line?);
-            paths.push(path);
+            path_to_path.insert(path, path);
         }
-        return Ok(paths);
+        return Ok(path_to_path);
     }
 }
-
