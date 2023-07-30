6eb94c4dea0f01c5cdcd903db5b723e8e1ea1ccf
Roben Kleene <contact@robenkleene.com>
2023-07-30 05:16:15 -0400

    Reverse the sort order

--- tests/data/dirs/generate.sh
+++ tests/data/dirs/generate.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 cd "$(dirname "$0")" || exit 1
 
-sorted=$(awk '{ print length, $0 }' < find.txt | sort -n -s | cut -d" " -f2-)
+sorted=$(awk '{ print length, $0 }' < find.txt | sort -nsr | cut -d" " -f2-)
 diff --unified <(echo "$sorted") <(echo "$sorted" | sed 's/\(.*\)changes/\1altered/') > patch.patch || true
 sed -i.bak '1s/.*/--- original/' patch.patch
 sed -i.bak '2s/.*/+++ modified/' patch.patch
