02aa1af129163974821867fd8cf0c23ded7bccf2
Roben Kleene <contact@robenkleene.com>
2023-07-30 05:44:55 -0400

    Only replace match at end

--- tests/data/dirs/generate.sh
+++ tests/data/dirs/generate.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 cd "$(dirname "$0")" || exit 1
 
 sorted=$(awk '{ print length, $0 }' < find.txt | sort -nsr | cut -d" " -f2-)
-diff --unified <(echo "$sorted") <(echo "$sorted" | sed 's/\(.*\)changes/\1altered/') > patch.patch || true
+diff --unified <(echo "$sorted") <(echo "$sorted" | sed 's/\(.*\)changes$/\1altered/') > patch.patch || true
 sed -i.bak '1s/.*/--- original/' patch.patch
 sed -i.bak '2s/.*/+++ modified/' patch.patch
 
