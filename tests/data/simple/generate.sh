4443175bf15817be0cae28db1b105285046b9c5d
Roben Kleene <contact@robenkleene.com>
2023-08-01 05:23:04 -0400

    Add new line messages

--- tests/data/simple/generate.sh
+++ tests/data/simple/generate.sh
@@ -4,9 +4,19 @@ set -euo pipefail
 
 cd "$(dirname "$0")" || exit 1
 
-diff --unified start.txt <(sed s/changes/altered/g start.txt) > patch.patch || true
+diff --unified find.txt <(sed s/changes/altered/g find.txt) > patch.patch || true
 
 sed -i.bak '1s/.*/--- original/' patch.patch
 sed -i.bak '2s/.*/+++ modified/' patch.patch
+
+# newline messages
+line_fix='5i\
+\\ No newline at end of file
+'
+sed -i.bak "${line_fix}" patch.patch
+line_fix='$a\
+\\ No newline at end of file
+'
+sed -i.bak "${line_fix}" patch.patch
 
 rm patch.patch.bak
