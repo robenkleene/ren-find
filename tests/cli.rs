c903cdbfefb5f9d43dd6ae0cfe41d81aacb39348
Roben Kleene <contact@robenkleene.com>
2023-08-02 05:36:10 -0400

    Fix path

--- tests/cli.rs
+++ tests/cli.rs
@@ -127,7 +127,7 @@ mod cli {
         println!("stdout = {:?}", String::from_utf8_lossy(&output.stdout));
         println!("stderr = {:?}", String::from_utf8_lossy(&output.stderr));
         assert!(!Path::exists(&file_path_dst));
-        let file_path_component_moved = "tests/data/mov/altered dir with spaces/altered dir with spaces two/altered file with spaces";
+        let file_path_component_moved = "altered dir with spaces/stays dir with spaces two/altered file with spaces";
         let file_path_moved = tmp_dir_path.join(file_path_component_moved);
         assert!(Path::exists(&file_path_moved));
         Ok(())
