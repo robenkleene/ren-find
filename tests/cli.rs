a5d3b422adb5289cdf3bb4a0aebe2615f1a99f6b
Roben Kleene <contact@robenkleene.com>
2023-08-04 05:18:04 -0400

    Rename mov to ren

--- tests/cli.rs
+++ tests/cli.rs
@@ -7,15 +7,15 @@ mod cli {
     use std::fs;
     use std::path::Path;
 
-    fn mov() -> Command {
-        Command::cargo_bin("mov").expect("Error invoking mov")
+    fn ren() -> Command {
+        Command::cargo_bin("ren").expect("Error invoking ren")
     }
 
     #[test]
     fn multiple_preview() -> Result<()> {
         let input = fs::read_to_string("tests/data/multiple/find.txt").expect("Error reading input");
         let result = fs::read_to_string("tests/data/multiple/patch.patch").expect("Error reading input");
-        mov()
+        ren()
             .current_dir("tests/data/multiple")
             .write_stdin(input)
             .args(&["changes", "altered"])
@@ -28,7 +28,7 @@ mod cli {
     #[test]
     fn missing_preview() -> Result<()> {
         let input = fs::read_to_string("tests/data/missing/find.txt").expect("Error reading input");
-        mov()
+        ren()
             .current_dir("tests/data/missing")
             .write_stdin(input)
             .args(&["missing", "replaced"])
@@ -42,7 +42,7 @@ mod cli {
     fn simple_preview() -> Result<()> {
         let input = fs::read_to_string("tests/data/simple/find.txt").expect("Error reading input");
         let result = fs::read_to_string("tests/data/simple/patch.patch").expect("Error reading input");
-        mov()
+        ren()
             .current_dir("tests/data/simple")
             .write_stdin(input)
             .args(&["changes", "altered"])
@@ -56,7 +56,7 @@ mod cli {
     fn nested_preview() -> Result<()> {
         let input = fs::read_to_string("tests/data/nested/find.txt").expect("Error reading input");
         let result = fs::read_to_string("tests/data/nested/patch.patch").expect("Error reading input");
-        mov()
+        ren()
             .current_dir("tests/data/nested")
             .write_stdin(input)
             .args(&["changes", "altered"])
@@ -70,7 +70,7 @@ mod cli {
     fn dirs_preview() -> Result<()> {
         let input = fs::read_to_string("tests/data/dirs/find.txt").expect("Error reading input");
         let result = fs::read_to_string("tests/data/dirs/patch.patch").expect("Error reading input");
-        mov()
+        ren()
             .current_dir("tests/data/dirs")
             .write_stdin(input)
             .args(&["changes", "altered"])
@@ -91,7 +91,7 @@ mod cli {
         let prefix = file_path_dst.parent().unwrap();
         std::fs::create_dir_all(prefix).unwrap();
         fs::copy(file_path, &file_path_dst).expect("Error copying file");
-        mov()
+        ren()
             .current_dir(tmp_dir_path)
             .write_stdin(input)
             .args(&["changes", "altered", "-w"])
@@ -117,7 +117,7 @@ mod cli {
         assert!(Path::exists(&file_path));
         fs::copy(file_path, &file_path_dst).expect("Error copying file");
         assert!(Path::exists(&file_path_dst));
-        let command = mov()
+        let command = ren()
             .current_dir(tmp_dir_path)
             .write_stdin(input)
             .args(&["changes", "altered", "-w"])
