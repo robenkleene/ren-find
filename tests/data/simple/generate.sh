8dd523f36e17d1331b150a59751bc7bf3ce0a7ba
Roben Kleene <contact@robenkleene.com>
2023-08-06 05:01:54 -0400

    Work on delete patch

--- tests/data/simple/generate.sh
+++ tests/data/simple/generate.sh
@@ -22,9 +22,13 @@ rm patch.patch.bak
 
 # Delete
 
-diff --unified find.txt <(echo "") > delete.patch || true
+diff --unified find.txt <(printf "") > delete.patch || true
 
 sed -i.bak '1s/.*/--- original/' delete.patch
 sed -i.bak '2s/.*/+++ modified/' delete.patch
+line_fix='$a\
+\\ No newline at end of file
+'
+sed -i.bak "${line_fix}" delete.patch
 
 rm delete.patch.bak
